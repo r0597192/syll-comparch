b1e8acd559024d7b0e6fe52c61133f065252ba4e
unknown <roel.vansteenberghe@gmail.com>
2014-02-19 23:05:10 +0100

    update ch1->3

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Some images are licensed differently, appropriate licenses are always mentioned.
 
 All imagery has been collected or crafted carefully, with respect to copyrights. However, if you think there is a copyright infringement, please do not hesitate to contact me, so I can investigate and possibly remove that content.
 
-
+http:// [just take me to a page with the exported files]
 
 ## Usage
 
--- render.bat
+++ render.bat
@@ -1,19 +1,19 @@
 echo ...omzetting naar html
-call asciidoctor "c:/syll-comparch/book_pcarch.asc" -o "c:/syll-comparch/pcarch.html"
+call asciidoctor "c:/syll-comparch/book_pcarch.asc" -o "c:/syll-comparch/pcarch.html" -a stylesheet="riak.css" -a linkcss
 echo ...omzetting naar html: done!
 
 echo PUBLISH TO GHDOCS
-copy  c:\syll-comparch\pcarch.html c:\syll-comparch-ghpages\syll-comparch\html5\pcarch.html
-robocopy c:\syll-comparch\ch01 c:\syll-comparch-ghpages\syll-comparch\html5\ch01 *.png *.jpg *.jpeg /mir
-robocopy c:\syll-comparch\ch02 c:\syll-comparch-ghpages\syll-comparch\html5\ch02 *.png *.jpg *.jpeg /mir 
-robocopy c:\syll-comparch\ch03 c:\syll-comparch-ghpages\syll-comparch\html5\ch03 *.png *.jpg *.jpeg /mir
+ copy  c:\syll-comparch\pcarch.html c:\syll-comparch-ghpages\syll-comparch\html5\pcarch.html
+ robocopy c:\syll-comparch\ch01 c:\syll-comparch-ghpages\syll-comparch\html5\ch01 *.png *.jpg *.jpeg /mir
+ robocopy c:\syll-comparch\ch02 c:\syll-comparch-ghpages\syll-comparch\html5\ch02 *.png *.jpg *.jpeg /mir 
+ robocopy c:\syll-comparch\ch03 c:\syll-comparch-ghpages\syll-comparch\html5\ch03 *.png *.jpg *.jpeg /mir
 
-echo ...omzetting naar docbook
-call asciidoctor "c:/syll-comparch/book_pcarch.asc" -o "c:/syll-comparch/pcarch.xml" -b docbook5
+ echo ...omzetting naar docbook
+ call asciidoctor "c:/syll-comparch/book_pcarch.asc" -o "c:/syll-comparch/pcarch.xml" -b docbook5
 echo ...omzetting naar docbook:done!
-echo ...omzetting naar PDF
-c:\fopub\asciidoctor-fopub\fopub c:\syll-comparch\pcarch.xml
-echo ... volledig klaar!
+ echo ...omzetting naar PDF
+ c:\fopub\asciidoctor-fopub\fopub c:\syll-comparch\pcarch.xml
+ echo ... volledig klaar!
 
 
 
